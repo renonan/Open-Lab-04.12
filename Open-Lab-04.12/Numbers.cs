833ef8acd9089642b86fd857d63069cbf1d623be
Roman <vojtus.roman@gmail.com>
2022-12-19 23:48:43 +0100

    ..

--- Open-Lab-04.12/Numbers.cs
+++ Open-Lab-04.12/Numbers.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 
 namespace Open_Lab_04._12
 {
@@ -6,7 +8,10 @@ namespace Open_Lab_04._12
     {
         public int MissingNum(int[] nums)
         {
-            throw new NotImplementedException();
+            int result =55-(nums.Sum());
+            return result;
+        
+
         }
     }
 }
